907f74fad397384fa644ecb26fcb49b1177ecca7
CyR1en <cyrien3519@gmail.com>
2021-05-24 18:08:51 -0600

    Prepare for cross-platform

--- src/main/python/util.py
+++ src/main/python/util.py
@@ -1,49 +1,47 @@
 import os.path
-import pathlib
 import sys
 
 from PyQt5.QtGui import QImage, QPixmap, QIcon
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 
 
-def get_resource(app_ctx: ApplicationContext, res_name):
-    print(os.getcwd())
-    path = app_ctx.get_resource(res_name)
+def get_fbs_resource(app_ctx: ApplicationContext, dir_name, res_name):
+    file = os.path.join(dir_name, res_name)
+    path = app_ctx.get_resource(file)
     print(path)
     return path
 
 
 def client_secret(app_ctx):
-    return get_resource(app_ctx, 'client_secret.json')
+    return get_fbs_resource(app_ctx, 'secret', 'client_secret.json')
 
 
 def token_dest():
-    return os.path.join(get_data_dir(), 'ephemeris-token.json')
+    return os.path.join(get_data_dir(), 'token.json')
 
 
-def get_data_dir() -> pathlib.Path:
-    home = pathlib.Path.home()
-
+def get_data_dir():
+    home = os.path.expanduser("~")
     if sys.platform == "win32":
-        return home / "AppData/Roaming"
+        home = os.path.join(home, 'AppData', 'Roaming')
     elif sys.platform == "linux":
-        return home / ".local/share"
+        home = os.path.join(home, '.local', 'share')
     elif sys.platform == "darwin":
-        return home / "Library/Application Support"
-
-
-def secret(file_name):
-    path = os.path.join(os.getcwd(), 'secret', file_name)
-    return path
+        home = os.path.join(home, 'Library', 'Application Support')
+    print(home)
+    home = os.path.join(home, 'Ephemeris')
+    if not os.path.isdir(home):
+        os.mkdir(home)
+    return home
 
 
 def get_QImage(app_ctx, res_name):
-    return QImage(get_resource(app_ctx, res_name))
+    return QImage(get_fbs_resource(app_ctx, 'res', res_name))
 
 
 def get_QPixmap(app_ctx, res_name):
-    return QPixmap(get_resource(app_ctx, res_name))
+    return QPixmap(get_fbs_resource(app_ctx, 'res', res_name))
 
 
 def get_QIcon(app_ctx, res_name):
-    return QIcon(get_resource(app_ctx, res_name))
+    return QIcon(get_fbs_resource(app_ctx, 'res', res_name))
