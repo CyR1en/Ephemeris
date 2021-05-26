3b4b821e668e396d05b4fde2998a03d6877470e8
CyR1en <cyrien3519@gmail.com>
2021-05-25 20:11:39 -0600

    Move ApplicationContext init

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,4 +1,10 @@
+import sys
+
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+
 from app import EphemerisApp
 
 if __name__ == '__main__':
-    ephemeris = EphemerisApp()
+    app_ctx = ApplicationContext()
+    ephemeris = EphemerisApp(app_ctx)
+    sys.exit(app_ctx.app.exec_())
